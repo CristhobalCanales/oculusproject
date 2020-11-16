3fe93d16b638ef665b089eeb13edb6b073d3054d
Cristhobal Canales <70823624+CristhobalCanales@users.noreply.github.com>
2020-11-16 20:38:34 -0300

    BD

--- models/Conexion.php
+++ models/Conexion.php
@@ -4,9 +4,13 @@ namespace models;
 
 class Conexion
 {
-  public static $user = "root";
-  public static $pass = "";
-  public static $URL = "mysql:host=localhost;dbname=optica_2020";
+  //public static $user = "root";
+  //public static $pass = "";
+  //public static $URL = "mysql:host=localhost;dbname=optica_2020";
+
+  public static $user = "uyz9yif3w44m9p94";
+  public static $pass = "EFw7kdT8yTtY9G0shhnk";
+  public static $URL = "mysql:host=bs68i6rxzigvxvgi5quy-mysql.services.clever-cloud.com;dbname=bs68i6rxzigvxvgi5quy";
 
   public static function conector()
   {
